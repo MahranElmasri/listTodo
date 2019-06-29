fce472ca3e596bd4cd157bb851e9bff4269cf6af
Mahran Elmasri <mahran_masri@yahoo.com>
2019-06-29 11:28:02 +0200

    add todo to list

---
+++ app.js
@@ -0,0 +1,16 @@
+const addForm = document.querySelector(".add");
+const todoList = document.querySelector(".todos");
+const generateTemplete = todo => {
+  const html = `<li
+    class="list-group-item d-flex justify-content-between align-items-center"
+  >
+    <span>${todo}</span>
+    <i class="far fa-trash-alt delete"></i>
+  </li>`;
+  todoList.innerHTML += html;
+};
+addForm.addEventListener("submit", e => {
+  e.preventDefault();
+  const todo = addForm.add.value;
+  generateTemplete(todo);
+});
