e6f57c78e289d456cab8a8cd483904800c9195f6
Mahran Elmasri <mahran_masri@yahoo.com>
2019-06-29 11:51:15 +0200

    add remove element

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector(".add");
 const todoList = document.querySelector(".todos");
+
 const generateTemplete = todo => {
   const html = `<li
     class="list-group-item d-flex justify-content-between align-items-center"
@@ -11,6 +12,14 @@ const generateTemplete = todo => {
 };
 addForm.addEventListener("submit", e => {
   e.preventDefault();
-  const todo = addForm.add.value;
-  generateTemplete(todo);
+  const todo = addForm.add.value.trim();
+  if (todo) {
+    generateTemplete(todo);
+    addForm.reset();
+  }
+});
+todoList.addEventListener("click", e => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
 });
