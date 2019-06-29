65fb35a39715824c71319500735474889d6d92a6
Mahran Elmasri <mahran_masri@yahoo.com>
2019-06-29 22:17:20 +0200

    add search element

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const addForm = document.querySelector(".add");
 const todoList = document.querySelector(".todos");
-
+const search = document.querySelector(".search input");
 const generateTemplete = todo => {
   const html = `<li
     class="list-group-item d-flex justify-content-between align-items-center"
@@ -22,4 +22,19 @@ todoList.addEventListener("click", e => {
   if (e.target.classList.contains("delete")) {
     e.target.parentElement.remove();
   }
+});
+
+const filterTodo = term => {
+  Array.from(todoList.children)
+    .filter(item => !item.textContent.includes(term))
+    .forEach(item => item.classList.add("filtered"));
+
+  Array.from(todoList.children)
+    .filter(item => item.textContent.includes(term))
+    .forEach(item => item.classList.remove("filtered"));
+};
+
+search.addEventListener("keyup", () => {
+  const term = search.value.trim();
+  filterTodo(term);
 });
